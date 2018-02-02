44160eab59f8da0f8b37a350e6efc04178419b27
Brooklin Myers <Brooklinjazz@hotmail.com>
2018-02-02 15:07:13 -0800

    add basic email validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
-
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  # validate email, must be unique and exist in a strict format
+  validates :email, presence: true, uniqueness: true, format:VALID_EMAIL_REGEX
+  # validate first and last name to exist
+  validates :first_name, :last_name, presence: true
 
   has_secure_password
 end

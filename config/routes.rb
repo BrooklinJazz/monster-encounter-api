fd6cd864829954e59f6557612a811a9446d2e4e7
Brooklin Myers <Brooklinjazz@hotmail.com>
2018-02-02 15:43:31 -0800

    configure routes for tokens

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resource :session, only: [:new, :create, :destroy]
+
+  namespace :api, defaults: {format: :json} do
+    namespace :v1 do
+      resources :tokens, only: [:create]
+    end
+  end
 end

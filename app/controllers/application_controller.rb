1d74d89264c57820160b96d5b7dd4098c21bbf93
Brooklin Myers <Brooklinjazz@hotmail.com>
2018-02-02 15:20:13 -0800

    add helper method user_signed_in?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,18 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||=User.find_by(id: session[:user_id])
   end
+  helper_method :current_user
+  # added
+  def user_signed_in?
+    # the following is to prevent the app from crashing if we have
+    # a 'user_id' in the session for a user that no loner exists
+    # in our database. Maybe the user was deleted or maybe our database
+    # was reseeded
+    if session[:user_id].present? && curent_user.nil?
+      # if you don't do this you will get an error
+      session[:user_id] = nil
+    end
+    session[:user_id].present?
+  end
+  helper_method :user_signed_in?
 end

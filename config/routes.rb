e24c2e194e459bba64f4440f80cc2268c131579b
Brooklin Myers <Brooklinjazz@hotmail.com>
2018-02-02 17:34:01 -0800

    generate api monsters controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'monsters/noah'
+
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resource :session, only: [:new, :create, :destroy]

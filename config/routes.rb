ddda225c61cf197dc49d87df0f46e7266c8afd3b
Brooklin Myers <Brooklinjazz@hotmail.com>
2018-02-03 13:31:03 -0800

    configure routes for combat using user

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   resources :combats
   # get 'monsters/noah'
 
-  resources :users
+  resources :users do
+    resources :combats
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resource :session, only: [:new, :create, :destroy]
 

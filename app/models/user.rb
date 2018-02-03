20deaf57493758b9e2e2e601aba5046552cfc4de
Brooklin Myers <Brooklinjazz@hotmail.com>
2018-02-03 12:35:24 -0800

    define user has many combats

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+
+  has_many :combats, dependent: :destroy
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   # validate email, must be unique and exist in a strict format
   validates :email, presence: true, uniqueness: true, format:VALID_EMAIL_REGEX
@@ -6,6 +9,8 @@ class User < ApplicationRecord
   validates :first_name, :last_name, presence: true
 
   has_secure_password
+
+
 
   def full_name
     "#{first_name} #{last_name}"

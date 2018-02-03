623b14b26a8c32e16f257e2d34ca4cefb84be646
Brooklin Myers <Brooklinjazz@hotmail.com>
2018-02-02 17:43:19 -0800

    configure monsters controller

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     namespace :v1 do
       resources :tokens, only: [:create]
+      resources :monsters, only: [:index]
     end
   end
 end

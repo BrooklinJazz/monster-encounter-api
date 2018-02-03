6b981da7494fe1a1aa5a27b2866fef619911988e
Brooklin Myers <Brooklinjazz@hotmail.com>
2018-02-03 11:57:01 -0800

    configured seeds file to add combats that are associated with a user

--- db/migrate/20180203192830_create_combats.rb
+++ db/migrate/20180203192830_create_combats.rb
@@ -3,7 +3,7 @@ class CreateCombats < ActiveRecord::Migration[5.1]
     create_table :combats do |t|
       t.string :name
       t.text :fight
-      t.references :user, foreign_key: true
+      t.references :user, foreign_key: true, index: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20180203192830) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "combats", "users"

f63f5a0c5aaa9da98a6686e44c8ec1489cba0f4e
Brooklin Myers <Brooklinjazz@hotmail.com>
2018-02-07 14:21:22 -0800

    configure CORS for delete action

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,8 @@ module BattleMasterRails
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        # NOTE determine if allowing :delete is a security issue
+        resource '*', :headers => :any, :methods => [:get, :post, :options, :delete]
       end
     end
 
